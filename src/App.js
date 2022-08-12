7681b6adaadf8c0ec5a8ae04389299d9d6de9474
pateldevansi14 <pateldevansi0@gmail.com>
2022-08-12 20:25:56 +0530

    devansi

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [query, setQuery] = useState({ q: "delhi" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+
+  return (
+    <div
+      className={`mx-auto max-w-screen mt-4 py-5 px-32 bg-gradient-to-br  h-fit `}
+    >
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
+
+      <ToastContainer autoClose={5000} theme="dark" newestOnTop={true} />
+    </div>
+  );
+}
+
+export default App;
